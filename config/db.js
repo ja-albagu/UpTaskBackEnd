ab55135a22d7390b943b0dbcac5118aedd5ed39d
ja-albagu <70991261+ja-albagu@users.noreply.github.com>
2020-09-09 14:12:55 -0500

    Quitar el notenv

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: 'variables.env' });
 const conectarDB = async () => {
     try {
         //await mongoose.connect(process.env.DB_MONGO, {
-        await mongoose.connect("mongodb+srv://root:root@cluster0.9o7pu.mongodb.net/uptask", {
+        await mongoose.connect(process.env.DB_MONGO, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
