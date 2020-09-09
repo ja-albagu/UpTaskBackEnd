aa8ed52c77e5805a1f2bf6aab6d5c8ffb0ca56b0
ja-albagu <70991261+ja-albagu@users.noreply.github.com>
2020-09-09 13:56:41 -0500

    Agregar ruta de MongoDB

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ require('dotenv').config({ path: 'variables.env' });
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_MONGO, {
+        //await mongoose.connect(process.env.DB_MONGO, {
+        await mongoose.connect("mongodb+srv://root:root@cluster0.9o7pu.mongodb.net/uptask", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
